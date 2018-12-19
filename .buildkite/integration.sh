b9244cc8741cf2864e82ef918bcbe39bceb31deb
Ari Fogel <ari@fogelti.me>
2018-12-19 00:02:49 -0800

    wait for batfish to come up

--- .buildkite/integration.sh
+++ .buildkite/integration.sh
@@ -24,13 +24,29 @@ fi
 
 echo "Using Batfish version ${BATFISH_VERSION}"
 
+# Start this as early as possible so that batfish has time to start up.
+java -cp "$ALLINONE_JAR" org.batfish.allinone.Main -runclient false -coordinatorargs "-templatedirs questions" &
+
 # Build and install pybatfish
 python3 -m virtualenv .venv
 . .venv/bin/activate
 pip install -e .[dev,test]
 
-# Start this as early as possible so that batfish has time to start up.
-java -cp "$ALLINONE_JAR" org.batfish.allinone.Main -runclient false -coordinatorargs "-templatedirs questions" &
+# Poll until we can connect to the container v1 endpoint
+while ! curl http://localhost:9997/
+do
+  echo "$(date) - waiting for Batfish v1 API to start"
+  sleep 1
+done
+echo "$(date) - connected to Batfish"
+
+# Poll until we can connect to the container v2 endpoint
+while ! curl http://localhost:9996/
+do
+  echo "$(date) - waiting for Batfish v2 API to start"
+  sleep 1
+done
+echo "$(date) - connected to Batfish"
 
 #### Running integration tests (require batfish)
 echo -e "\n  ..... Running python integration with batfish"

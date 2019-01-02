911a63116646cd854b67bc3b484c66d7095418b2
Ari Fogel <ari@fogelti.me>
2019-01-02 12:07:08 -0800

    try alternate download syntax

--- .buildkite/download_artifacts.sh
+++ .buildkite/download_artifacts.sh
@@ -5,8 +5,8 @@ S3_BUCKET="s3://batfish-build-artifacts-arifogel"
 export BATFISH_TAR="artifacts/batfish/dev.tar"
 BATFISH_DIR="$(dirname "${BATFISH_TAR}")"
 mkdir -p "${BATFISH_DIR}"
-buildkite-agent artifact download "${S3_BUCKET}/${BATFISH_TAR}" "${BATFISH_DIR}/"
 pushd "${BATFISH_DIR}"
+buildkite-agent artifact download "${S3_BUCKET}/${BATFISH_TAR}"
 tar -x --no-same-owner -f dev.tar
 [ -n "$(cat tag)" ]
 popd

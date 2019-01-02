715dc32d30903837989beb22aa7b3b71c7099a9c
Ari Fogel <ari@fogelti.me>
2019-01-02 10:38:59 -0800

    add version, fix tag

--- .buildkite/deploy_artifacts.sh
+++ .buildkite/deploy_artifacts.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 set -e
 set -x
+S3_BUCKET="s3://batfish-build-artifacts-arifogel"
 PYBATFISH_TAG="$(git rev-parse HEAD)"
+[ -n "${PYBATFISH_TAG}" ]
+PYBATFISH_VERSION="$(python setup.py --version)"
+[ -n "${PYBATFISH_VERSION}" ]
 ARTIFACTS_DIR=artifacts/pybatfish
 ARTIFACT_TAR="${PYBATFISH_TAG}.tar"
 mkdir -p "${ARTIFACTS_DIR}"
 cd "${ARTIFACTS_DIR}"
-echo "${BATFISH_TAG}" > tag
-tar -cf "${ARTIFACT_TAR}" tag
+echo "${PYBATFISH_TAG}" > tag
+echo "${PYBATFISH_VERSION}" > version
+tar -cf "${ARTIFACT_TAR}" tag version
 ln "${ARTIFACT_TAR}" dev.tar
-buildkite-agent artifact upload "${ARTIFACT_TAR}" s3://batfish-build-artifacts-arifogel
-buildkite-agent artifact upload dev.tar s3://batfish-build-artifacts-arifogel
+buildkite-agent artifact upload "${ARTIFACT_TAR}" "${S3_BUCKET}"
+buildkite-agent artifact upload dev.tar "${S3_BUCKET}"
 

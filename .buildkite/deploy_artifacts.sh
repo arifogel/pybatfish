8dfe904c23f4cc5c43335d30e972270cd4a13d0d
Ari Fogel <ari@fogelti.me>
2019-01-08 17:18:26 -0800

    only upload once

--- .buildkite/deploy_artifacts.sh
+++ .buildkite/deploy_artifacts.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
 set -x
-S3_BUCKET="s3://batfish-build-artifacts-arifogel"
+S3_BUCKET_NAME="batfish-build-artifacts-arifogel"
+S3_BUCKET="s3://${S3_BUCKET_NAME}"
 PYBATFISH_TAG="$(git rev-parse HEAD)"
 [ -n "${PYBATFISH_TAG}" ]
 PYBATFISH_VERSION="$(python setup.py --version)"
@@ -19,6 +20,11 @@ echo "${PYBATFISH_TAG}" > tag
 echo "${PYBATFISH_VERSION}" > version
 tar -cf "${ARTIFACT_TAR}" integration_tests.tgz jupyter_notebooks.tgz "$(basename "${BDIST_WHEEL}")" tag version
 ln "${ARTIFACT_TAR}" dev.tar
-buildkite-agent artifact upload "${ARTIFACT_TAR}" "${S3_BUCKET}/${ARTIFACTS_DIR}/"
-buildkite-agent artifact upload dev.tar "${S3_BUCKET}/${ARTIFACTS_DIR}/"
+ARTIFACT_KEY="${ARTIFACTS_DIR}/${ARTIFACT_TAR}"
+if aws s3api head-object --bucket "${S3_BUCKET_NAME}" --key "${ARTIFACT_KEY}"; then
+  echo "Skipping upload since artifact already exists for this commit"
+else
+  buildkite-agent artifact upload "${ARTIFACT_TAR}" "${S3_BUCKET}/${ARTIFACTS_DIR}/"
+  buildkite-agent artifact upload dev.tar "${S3_BUCKET}/${ARTIFACTS_DIR}/"
+fi
 

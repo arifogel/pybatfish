e5ce4d18a0b7cbe8b72774d970463a5a5411e815
Ari Fogel <ari@fogelti.me>
2019-01-08 17:25:39 -0800

    suppress

--- .buildkite/deploy_artifacts.sh
+++ .buildkite/deploy_artifacts.sh
@@ -21,7 +21,7 @@ echo "${PYBATFISH_VERSION}" > version
 tar -cf "${ARTIFACT_TAR}" integration_tests.tgz jupyter_notebooks.tgz "$(basename "${BDIST_WHEEL}")" tag version
 ln "${ARTIFACT_TAR}" dev.tar
 ARTIFACT_KEY="${ARTIFACTS_DIR}/${ARTIFACT_TAR}"
-if aws s3api head-object --bucket "${S3_BUCKET_NAME}" --key "${ARTIFACT_KEY}"; then
+if aws s3api head-object --bucket "${S3_BUCKET_NAME}" --key "${ARTIFACT_KEY}" >& /dev/null; then
   echo "Skipping upload since artifact already exists for this commit"
 else
   buildkite-agent artifact upload "${ARTIFACT_TAR}" "${S3_BUCKET}/${ARTIFACTS_DIR}/"
